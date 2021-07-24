21fe048696155fbc0cd70419ad6d79ca2c653bfb
Victor Kowalski Martins <victor.kowalski.m@gmail.com>
2021-07-24 17:58:40 -0300

    Matrizes

--- setup.m
+++ setup.m
@@ -2,8 +2,9 @@ clear
 clc
 close all
 
-dx = 3;
+dx = 3; % discretização escolhida
 
+%%% Pontos do desenho
 pontos.A.coor = [0  0 ]; pontos.A.con = 1; % A
 pontos.B.coor = [5  0 ]; pontos.B.con = 0; % B
 pontos.C.coor = [9  0 ]; pontos.C.con = 0; % C
@@ -13,129 +14,121 @@ pontos.F.coor = [36 18]; pontos.F.con = 0; % F
 pontos.G.coor = [36 -5]; pontos.G.con = 0; % G
 pontos.H.coor = [65  0]; pontos.H.con = 0; % H
 
-nos = 0;
+coor = pontos.A.coor; con = []; nos = 1; % inicialização de matrizes de
+    % coordenadas e conectividade
 
-[AB, nAB] = calcula_pontos_internos(pontos.A.coor, pontos.B.coor, dx);
-coor = AB;
-con = [(1:nAB-1)', (2:nAB)'];
-nos = nos + nAB;
-pontos.B.con = nos;
-
-ps = ['B', 'C', 'D', 'E', 'H'];
+%%% Insere viga horizontal nas matrizes de coord. e conec.
+ps = ['A', 'B', 'C', 'D', 'E', 'H'];
 for i=1:length(ps)-1
     [pIn, NpIn] = calcula_pontos_internos( ...
         pontos.(ps(i)).coor, pontos.(ps(i+1)).coor, dx);
     pIn = pIn(2:end, :); NpIn = NpIn - 1;
-    coor = [coor; pIn];
+    coor = [coor; pIn]; 
     con = [con; nos+[(0:NpIn-1)', (1:NpIn)']];
     nos = nos + NpIn;
     pontos.(ps(i+1)).con = nos;
 end
+vigaHor = 1:size(con, 1);
 
+%%% Insere viga vertical nas matrizes de coord. e conec.
 for p=['G', 'F']
     [pIn, NpIn] = calcula_pontos_internos( ...
         pontos.E.coor, pontos.(p).coor, dx);
     pIn = pIn(2:end, :); NpIn = NpIn - 1;
     coor = [coor; pIn];
-    con = [con; [pontos.E.con nos+1]];
+    con = [con; [pontos.E.con nos+1]]; 
     con = [con; nos+[(1:NpIn-1)', (2:NpIn)']];
     nos = nos + NpIn;
     pontos.(p).con = nos;
 end
+vigaVer = vigaHor(2)+1:size(con, 1);
 
+%%% Insere treliças nas matrizes de coord. e conec.
 for p = ['B', 'D', 'H']
     con(end+1, :) = [pontos.(p).con pontos.F.con];
 end
+trelicas = vigaVer(2)+1:size(con, 1);
+
+% plot_struct(coor, con)
+
+%%% Quantidades de nós, elementos e G.L.
+Nod = size(coor, 1);
+Nel = size(con, 1);
+Ngdl = 3*Nel;
+
+%%% Props. dos elementos
+data.E = 210e9*ones(Nel, 1);
+data.rho = 7600*ones(Nel, 1);
+
+data.A = zeros(Nel, 1);
+data.A(vigaHor) = 1.8*0.9; 
+data.A(vigaVer) = 0.9*0.9;
+data.A(trelicas) = pi*0.05^2/4;
+
+data.L = zeros(Nel, 1);
+data.Q = zeros(Nel, 1);
+
+for e = 1:Nel
+    
+    x1 = coor(con(e, 1), 1);
+    x2 = coor(con(e, 2), 1);
+    
+    y1 = coor(con(e, 1), 2);
+    y2 = coor(con(e, 2), 2);
+    
+    data.L(e) = sqrt((x2-x1)^2 + (y2-y1)^2);
+    data.Q(e) = atan2(y2-y1, x2-x1)*180/pi;    
+end
 
 
-% [pontos, Npontos] = calcula_pontos_internos( ...
-%     pG.coor, pontos_hor(i+1).coor, dx);
-% pontos = pontos(2:end, :); Npontos = Npontos - 1;
-% coor = [coor; pontos];
-% con = [con; (nos-1)+[(1:Npontos-1)', (2:Npontos)']];
-% nos = nos + Npontos;
+%%% Funções para cálculo das matrizes de rigidez, massa e transformação
+Ke = @(E, A, I, L)...
+    [ ...
+     1  0  0 -1  0  0;
+     0  0  0  0  0  0;
+     0  0  0  0  0  0;
+    -1  0  0  1  0  0;
+     0  0  0  0  0  0;
+    ] * E*A/L + ...
+    [ ...
+     0      0      0      0     0     0     ;
+     0      12     6*L    0    -12    6*L   ;
+     0      6*L    4*L^2  0    -6*L   2*L^2 ;
+     0      0      0      0     0     0     ;
+     0     -12    -6*L    0     12   -6*L   ;
+     0      6*L    2*L^2  0     -6*L  4*L^2 ;
+    ] * E*I/L^3 ;
 
-% [pontos, Npontos] = calcula_pontos_internos(pB.coor, pC.coor, dx);
-% pontos = pontos(2:end, :); Npontos = Npontos - 1;
-% coor = [coor; pontos];
-% con = [con; (nos-1)+[(1:Npontos-1)', (2:Npontos)']];
-% nos = nos + Npontos;
-% 
-% [CD, Npontos] = calcula_pontos_internos(pB.coor, pC.coor, dx);
-% pontos = pontos(2:end, :); Npontos = Npontos - 1;
-% coor = [coor; CD];
-% con = [con; (nos-1)+[(1:Npontos-1)', (2:Npontos)']];
-% nos = nos + Npontos;
+Me = @(rho, A, L)...
+    [ ...
+     2  0  0  1  0  0;
+     0  0  0  0  0  0;
+     0  0  0  0  0  0;
+     1  0  0  2  0  0;
+     0  0  0  0  0  0;
+    ] * rho*A*L/6 + ...
+    [ ...
+     0       0       0       0      0      0     ;
+     0       156     22*L    0      54    -13*L   ;
+     0       22*L    4*L^2   0      13*L  -3*L^2 ;
+     0       0       0       0      0      0     ;
+     0       54      13*L    0      156   -22*L   ;
+     0      -13*L   -3*L^2   0     -22*L   4*L^2 ;
+    ] * rho*A*L/420 ;
 
+T = @(theta)...
+    [
+     cos(theta) -sin(theta)  0          0           0           0;
+     sin(theta)  cos(theta)  0          0           0           0;
+     0           0           1          0           0           0;
+     0           0           0          cos(theta) -sin(theta)  0;
+     0           0           0          sin(theta)  cos(theta)  0;
+     0           0           0          0           0           1
+    ];
 
-% coor_viga_hor = calcula_pontos_internos(pA.coor, pH.coor, dx);
-% Ncoor_viga_hor = size(coor_viga_hor, 1);
-% for i=2:Ncoor_viga_hor
-%     coor = [coor coor_viga_hor(i,:)];
-%     con = [con; [i-1 i]];
-%     for ponto=[pB pC pD pE pH]
-%         if isequal(coor_viga_hor(i,:), ponto.coor)
-%             ponto.con = i;
-%         end
-%     end
-% end
-% 
-% pG.con = i+1;
-% coor = [coor pG.coor];
-% coor_viga_ver = calcula_pontos_internos(pG.coor, pF.coor, dx);
-% Ncoor_viga_ver = size(coor_viga_ver, 1);
-% pulouE = 0;
-% for i=2:Ncoor_viga_ver
-%     if isequal(coor_viga_ver(i,:), pE.coor)
-%         con = [con; [con(end, 2) pE.con]];
-%         pulouE = 1;
-%     else
-%         coor = [coor coor_viga_ver(i,:)];
-%         con = [con; [con(end, 2) Ncoor_viga_hor+i-pulouE]];
-%     end
-% end
-
-% 
-% con_viga_hor = (1:Ncoor_viga_hor) + (idx-1);
-% coor = [coor; coor_viga_hor];
-% con =  [con; [con_viga_hor(1:end-1)', con_viga_hor(2:end)']];
-% elems_viga_hor = [idx idx+Ncoor_viga_hor];
-% idx = elems_viga_hor(2);
-% 
-% coor_viga_ver = calcula_pontos_internos(pontos.G, pontos.F, dx);
-% Ncoor_viga_ver = size(coor_viga_ver, 1);
-% con_viga_ver = (1:Ncoor_viga_ver) + (idx-1);
-% coor = [coor; coor_viga_ver];
-% con =  [con; [con_viga_ver(1:end-1)', con_viga_ver(2:end)']];
-% elems_viga_ver = [idx idx+Ncoor_viga_ver];
-% idx = elems_viga_ver(2);
+Kg = zeros(Ngdl);
+Mg = zeros(Ngdl);
 
 
-% pontos_desenho = [ 
-%     0  0 ; % A
-%     5  0 ; % B
-%     9  0 ; % C
-%     18 0 ; % D
-%     36 0 ; % E
-%     36 18; % F
-%     36 -5; % G
-%     65  0; % H
-% ];
 
-% con = [
-%     
-%     % Viga horizontal
-%     1  8  1;
-%     
-%     % Viga vertical
-%     7  6 1;
-%     
-%     % Treliças
-%     2  6  0;
-%     4  6  0;
-%     6  8  0;
-% ];
-
-%[coor, con] = discretiza(coorDesenho, conDesenho);
-plot_struct(coor, con)
 
